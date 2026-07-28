20dff219b45ddcade98402c59cbeeb450852b820
santhoshsivkumar <santhosh.sivkumar@gmail.com>
2026-07-28 23:06:23 +0530

    fix config

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -3,15 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAPjkDsrHaafatVPJbG2ebk9Cbp219VOnE",
+  authDomain: "docs-app-270e7.firebaseapp.com",
+  projectId: "docs-app-270e7",
+  storageBucket: "docs-app-270e7.appspot.com",
+  messagingSenderId: "1075659271026",
+  appId: "1:1075659271026:web:009db771d4bc424dfe8beb",
+  measurementId: "G-KGHVX3SDRL",
 };
 
-export const app      = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
-export const storage  = getStorage(app);
+export const storage = getStorage(app);
